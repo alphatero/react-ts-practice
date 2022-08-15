eee4e3379e618f1ad32be66a7d7daf3fb37973e7
lucentgit <alphatero@icloud.com>
2022-08-15 23:18:01 +0800

    fix: 🐛 remove router

--- src/App.tsx
+++ src/App.tsx
@@ -10,18 +10,15 @@ function App() {
         href="https://github.com/alphatero/react-ts-practice/"
         direction="right"
       />
-      <ul className="flex justify-center space-x-4 p-4 ">
+      {/* <ul className="flex justify-center space-x-4 p-4 ">
         <li>
           <NavLink to="/day1">Day 1 - 匯率兌換</NavLink>
         </li>
         <li>
           <NavLink to="/day2">Day 2 - TODOLIST</NavLink>
         </li>
-      </ul>
-      <Routes>
-        <Route path="/day1" element={<Day1 />} />
-        <Route path="/day2" element={<Day2 />} />
-      </Routes>
+      </ul> */}
+      <Day2 />
     </div>
   );
 }
